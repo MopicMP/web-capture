9fbc4cb9da25da4218cd783266c7452095167f58
RaccoNet <zavolokinyegor@gmail.com>
2026-03-03 14:40:51 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="web-capture",
+    version="1.0.0",
+    description="Save web pages as local HTML/PDF",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="raccoonette",
+    url="https://github.com/MopicMP/web-capture",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
